18e069ebe03804652d0dc63964d6b0fda3d38e63
huyifan <yifan_hu@zju.edu.cn>
2019-11-28 15:02:46 +0800

    feat: 新增mnist_cluster_demo

---
+++ examples/mnist_cluster_demo/fl_client.py
@@ -0,0 +1,35 @@
+import torch
+import os
+from torchvision import datasets, transforms
+from tianshu_fl.core.strategy import WorkModeStrategy
+from tianshu_fl.core.trainer import Trainer
+from tianshu_fl.core.job_detector import JobDetector
+from torch import nn
+import torch.nn.functional as F
+
+
+SERVER_URL = "http://127.0.0.1:9763"
+CLIENT_IP = "127.0.0.1"
+CLIENT_PORT = 8081
+CLIENT_ID = 0
+
+def start_trainer(work_mode, client_id, data):
+    if work_mode == WorkModeStrategy.WORKMODE_STANDALONE:
+        Trainer(work_mode, data, client_id, None, None, 3).start()
+    else:
+        Trainer(work_mode, data, client_id, CLIENT_IP, CLIENT_PORT, SERVER_URL, 3).start()
+    #print(os.path.abspath("."))
+
+if __name__ == "__main__":
+    mnist_data = datasets.MNIST("./mnist_data", download=True, train=True, transform=transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.13066062,), (0.30810776,))
+    ]))
+
+    #start_trainer(WorkModeStrategy.WORKMODE_STANDALONE, CLIENT_ID, mnist_data)
+
+    start_trainer(WorkModeStrategy.WORKMODE_CLUSTER, CLIENT_ID, mnist_data)
+
+
+
+
---
+++ examples/mnist_cluster_demo/fl_server.py
@@ -0,0 +1,18 @@
+
+from tianshu_fl.core.server import TianshuFlStandaloneServer, TianshuFlClusterServer
+from tianshu_fl.core.job_detector import JobDetector
+from tianshu_fl.core.strategy import WorkModeStrategy, FedrateStrategy
+
+WORK_MODE = WorkModeStrategy.WORKMODE_CLUSTER
+FEDERATE_STRATEGY = FedrateStrategy.FED_AVG
+IP = '0.0.0.0'
+PORT = 9763
+API_VERSION = '/api/version'
+
+if __name__ == "__main__":
+
+    if WORK_MODE == WorkModeStrategy.WORKMODE_STANDALONE:
+        TianshuFlStandaloneServer(FEDERATE_STRATEGY).start()
+    else:
+        TianshuFlClusterServer(FEDERATE_STRATEGY, IP, PORT, API_VERSION).start()
+
